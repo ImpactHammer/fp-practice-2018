1745000141649f4cfcf31788bf5dfd337ac6acb9
gleps <ImpactHammer@yandex.ru>
2019-02-13 00:53:09 +0300

    Task1

--- Task1_1.hs
+++ Task1_1.hs
@@ -2,26 +2,64 @@ module Task1_1 where
 
 import Todo(todo)
 
-data Term = IntConstant{ intValue :: Int }           -- числовая константа
-            | Variable{ varName :: String }          -- переменная
-            | BinaryTerm{ lhv :: Term, rhv :: Term } -- бинарная операция
+data Op =   Plus
+            | Minus
+            | Mult
+            deriving(Show,Eq)
+
+opfunTerm :: Op -> (Term -> Term -> Term)
+opfunTerm Plus  = (|+|)
+opfunTerm Minus = (|-|)
+opfunTerm Mult  = (|*|)
+
+opfunInt :: Op -> (Int -> Int -> Int)
+opfunInt Plus  = (+)
+opfunInt Minus = (-)
+opfunInt Mult  = (*)
+
+data Term = IntConstant{ intValue :: Int }  -- числовая константа
+            | Variable{ varName :: String } -- переменная
+            | BinaryTerm{ lhv :: Term,
+                          rhv :: Term,
+                          op  :: Op }       -- бинарная операция
             deriving(Show,Eq)
 
 -- Для бинарных операций необходима не только реализация, но и адекватные
 -- ассоциативность и приоритет
 (|+|) :: Term -> Term -> Term
-(|+|) l r = todo
+(|+|) l r = BinaryTerm l r Plus
 (|-|) :: Term -> Term -> Term
-(|-|) l r = todo
+(|-|) l r = BinaryTerm l r Minus
 (|*|) :: Term -> Term -> Term
-(|*|) l r = todo
+(|*|) l r = BinaryTerm l r Mult
+
+infixl 6 |+|
+infixl 6 |-|
+infixl 7 |*|
 
 -- Заменить переменную `varName` на `replacement`
 -- во всём выражении `expression`
-replaceVar :: String -> Term -> Term -> Term
-replaceVar varName replacement expression = todo
+type VarName = String
+type Replacement = Term
+type Expression = Term
+replaceVar :: VarName -> Replacement -> Expression -> Term
+replaceVar _ _ (IntConstant i) = IntConstant i
+replaceVar varName replacement (Variable v) =
+    if v == varName then replacement else (Variable v)
+replaceVar varName replacement (BinaryTerm l r op) = 
+    BinaryTerm (rep l) (rep r) op
+    where rep = replaceVar varName replacement
 
 -- Посчитать значение выражения `Term`
 -- если оно состоит только из констант
 evaluate :: Term -> Term
-evaluate expression = todo
+evaluate (BinaryTerm (IntConstant l) (IntConstant r) op) =
+    IntConstant (opfunInt op l r)
+evaluate (BinaryTerm (IntConstant 0) _ Mult) = IntConstant 0
+evaluate (BinaryTerm _ (IntConstant 0) Mult) = IntConstant 0
+evaluate (BinaryTerm (IntConstant 1) r Mult) = evaluate r
+evaluate (BinaryTerm l (IntConstant 1) Mult) = evaluate l
+evaluate (BinaryTerm (IntConstant 0) r _)    = evaluate r
+evaluate (BinaryTerm l (IntConstant 0) _)    = evaluate l
+evaluate (BinaryTerm l r op) = evaluate (opfunTerm op (evaluate l) (evaluate r))
+evaluate expression = expression
--- Task1_2.hs
+++ Task1_2.hs
@@ -1,10 +1,19 @@
 module Task1_2 where
 
 import Todo(todo)
+import Prelude hiding (gcd, sin)
 
 -- синус числа (формула Тейлора)
 sin :: Double -> Double
-sin x = todo
+sin x = sin' x (x**3) (3 :: Double) (6 :: Double) x
+    where
+    sin' :: Double -> Double -> Double -> Double -> Double -> Double
+    sin' x x_pow n n_fact result
+        | (abs sin_entry) > 0 = sin'
+        x (x_pow * x * x) (n + 2) (n_fact * (n + 2) * (n + 1)) (result + sin_entry)
+        | otherwise = result
+        where
+        sin_entry = (-1)**((n - 1) / 2) * x_pow / n_fact
 
 -- косинус числа (формула Тейлора)
 cos :: Double -> Double
@@ -12,11 +21,15 @@ cos x = todo
 
 -- наибольший общий делитель двух чисел
 gcd :: Integer -> Integer -> Integer
-gcd x y = todo
+gcd x y
+    | y == 0 = x
+    | otherwise = gcd y (x `mod` y)
 
 -- существует ли полный целочисленный квадрат в диапазоне [from, to)?
 doesSquareBetweenExist :: Integer -> Integer -> Bool
-doesSquareBetweenExist from to = todo
+doesSquareBetweenExist from to = bound (to - 1) - bound from > 0
+    where
+    bound = floor . sqrt . fromIntegral
 
 -- является ли дата корректной с учётом количества дней в месяце и
 -- вискокосных годов?
@@ -26,11 +39,22 @@ isDateCorrect day month year = todo
 -- возведение числа в степень, duh
 -- готовые функции и плавающую арифметику использовать нельзя
 pow :: Integer -> Integer -> Integer
-pow x y = todo
+pow x y
+    | y > 0 = pow' 1 x y
+    | otherwise = 0
+    where
+    pow' prod _ 0 = prod
+    pow' prod x y = pow' (prod * x) x (y - 1)
 
 -- является ли данное число простым?
 isPrime :: Integer -> Bool
-isPrime x = todo
+isPrime x = isPrime' x 2 (ceiling . sqrt . fromIntegral $ x)
+    where
+    isPrime' :: Integer -> Integer -> Integer -> Bool
+    isPrime' x n last
+        | n == last = (x `mod` last) /= 0
+        | (x `mod` n) == 0 = False
+        | otherwise = isPrime' x (n + 1) last
 
 type Point2D = (Double, Double)
 
