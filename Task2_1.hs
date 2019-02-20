2576a193c6a8885d026ecc7dffa102862dbdad97
gleps <ImpactHammer@yandex.ru>
2019-02-20 19:54:56 +0300

    Task 2 #0

--- Task2_1.hs
+++ Task2_1.hs
@@ -1,43 +1,111 @@
 module Task2_1 where
 
 import Todo(todo)
+import Prelude hiding (lookup)
 
 -- Ассоциативный массив на основе бинарного дерева поиска
 -- Ключи - Integer, значения - произвольного типа
-data TreeMap v = ChangeMe 
+data TreeMap v = Empty | Node { key   :: Integer,
+                                value :: v,
+                                left  :: TreeMap v,
+                                right :: TreeMap v }
+                                deriving (Show)
 
 -- Пустое дерево
 emptyTree :: TreeMap v
-emptyTree = todo
+emptyTree = Empty
 
 -- Содержится ли заданный ключ в дереве?
 contains :: TreeMap v -> Integer -> Bool
-contains t k = todo
+contains Empty k = False
+contains (Node key _ left right) k
+    | k == key = True
+    | k < key = contains left k
+    | otherwise = contains right k
 
 -- Значение для заданного ключа
 lookup :: Integer -> TreeMap v -> v
-lookup k t = todo
+lookup k Empty = error "No such key"
+lookup k (Node key value left right)
+    | k == key = value
+    | k < key = lookup k left
+    | otherwise = lookup k right
 
--- Вставка пары (ключ, значение) в дерево
+-- Вставка пары (ключ, значение) в деревопр
 insert :: (Integer, v) -> TreeMap v -> TreeMap v
-insert (k, v) t = todo
+insert (k, v) Empty = Node k v Empty Empty
+insert (k, v) (Node key value left right)
+    | k == key = (Node key v left right)
+    | k < key = (Node key value (newSubtree left) right)
+    | otherwise = (Node key value left (newSubtree right))
+        where newSubtree = insert (k, v)
 
 -- Удаление элемента по ключу
 remove :: Integer -> TreeMap v -> TreeMap v
-remove i t = todo
+remove i Empty = Empty
+remove i (Node k v l r)
+    | i == k = case (l, r) of
+        (_, Empty) -> l
+        (Empty, _) -> r
+        _ -> Node (key minNode) (value minNode) l resid
+                where
+                residAndMin = removeAndGetMin r
+                resid = fst residAndMin
+                minNode = snd residAndMin
+    | i < k = (Node k v (newSubtree l) r)
+    | otherwise = (Node k v l (newSubtree r))
+        where newSubtree = remove i
+
+removeAndGetMin :: TreeMap v -> (TreeMap v, TreeMap v)
+removeAndGetMin (Node k v l r) = case (left l) of
+    Empty -> (Node k v (right l) r, l)
+    _ -> ((Node k v (fst $ removeAndGetMin l) r), Empty)
 
 -- Поиск ближайшего снизу ключа относительно заданного
 nearestLE :: Integer -> TreeMap v -> (Integer, v)
-nearestLE i t = todo
+nearestLE i Empty = error "Empty tree"
+nearestLE i t = case tmp of
+    Empty -> error "Lowest key value"
+    _
+        | key tmp == i -> (key tmp, value tmp)
+        | otherwise -> (key resultNode, value resultNode)
+            where 
+            resultNode = case left tmp of
+                Empty -> tmp
+                _ -> maxNode (left tmp)
+    where
+    tmp = nearestLE' i t Empty
+
+nearestLE' :: Integer -> TreeMap v -> TreeMap v -> TreeMap v
+nearestLE' i Empty rightChosen = rightChosen
+nearestLE' i t@(Node k _ l r) rightChosen
+    | i == k = t
+    | i < k = nearestLE' i l rightChosen
+    | otherwise = nearestLE' i r t
+
+maxNode :: TreeMap v -> TreeMap v
+maxNode Empty = Empty
+maxNode t@(Node _ _ _ Empty) = t
+maxNode t = maxNode $ right t
 
 -- Построение дерева из списка пар
 treeFromList :: [(Integer, v)] -> TreeMap v
-treeFromList lst = todo
+treeFromList [] = Empty
+treeFromList (x:xs) = insert x $ treeFromList xs
 
 -- Построение списка пар из дерева
 listFromTree :: TreeMap v -> [(Integer, v)]
-listFromTree t = todo
+listFromTree Empty = []
+listFromTree (Node k v l r) = 
+    (k, v):((listFromTree l) ++ (listFromTree r))
 
--- Поиск k-той порядковой статистики дерева 
+-- Поиск k-той порядковой статистики дерева
+size :: TreeMap v -> Integer
+size Empty = 0
+size t = (size $ left t) + (size $ right t) + 1
 kMean :: Integer -> TreeMap v -> (Integer, v)
-kMean i t = todo
+kMean _ Empty = error "No such rank"
+kMean i (Node k v l r)
+    | i == size l = (k, v)
+    | i < size l = kMean i l
+    | otherwise = kMean (i - size l - 1) r
--- Task2_2.hs
+++ Task2_2.hs
@@ -6,13 +6,17 @@ import Prelude hiding (foldl, foldr, unfoldr, map, concatMap,
     filter, maxBy, minBy, reverse, sum, product, elem)
 
 foldl :: (b -> a -> b) -> b -> [a] -> b
-foldl = todo
+foldl f b [] = b
+foldl f b (x:xs) = foldl f (f b x) xs
 
 foldr :: (a -> b -> b) -> b -> [a] -> b
-foldr = todo
+foldr f b [] = b
+foldr f b (x:xs) = f x (foldr f b xs)
 
 unfoldr :: (b -> Maybe (a, b)) -> b -> [a]
-unfoldr = todo
+unfoldr f b = case f b of
+    Nothing -> []
+    Just (a0, b0) -> a0:(unfoldr f b0)
 
 -- Сумма всех элементов списка (пример)
 sum :: [Integer] -> Integer
@@ -24,37 +28,64 @@ reverse lst = foldl f [] lst where f t h = h:t
 
 -- Отображение элементов списка
 map :: (a -> b) -> [a] -> [b]
-map = todo
+map f lst = foldr f0 [] lst where f0 h t = (f h):t
 
 -- Произведение всех элементов списка
 product :: [Integer] -> Integer
-product = todo
+product lst = foldr (*) 1 lst
 
 -- Выделение из списка Maybe всех существующих значений
 catMaybes :: [Maybe a] -> [a]
-catMaybes = todo
+catMaybes [] = []
+catMaybes lst = foldr f [] lst
+    where 
+    f x xs = case x of
+        Nothing -> xs
+        Just y -> y:xs
 
 -- Диагональ матрицы
 diagonal :: [[a]] -> [a]
-diagonal = todo
+diagonal lst = zipWith (!!) lst [0..(length $ head lst) - 1]
 
 -- Фильтр для всех элементов, не соответствующих предикату
 filterNot :: (a -> Bool) -> [a] -> [a]
-filterNot = todo
+filterNot p lst = foldr f [] lst 
+    where 
+    f x xs
+        | p x = xs
+        | otherwise = x:xs
 
 -- Поиск элемента в списке
 elem :: (Eq a) => a -> [a] -> Bool
-elem = todo
+elem e lst = foldr f False lst
+    where
+    f x xs = (x == e) || xs
 
 -- Список чисел в диапазоне [from, to) с шагом step
 rangeTo :: Integer -> Integer -> Integer -> [Integer]
-rangeTo from to step = todo
+rangeTo from to step = unfoldr f from
+    where
+    f b
+        | b < to = Just (b, b + step)
+        | otherwise = Nothing
 
 -- Конкатенация двух списков
 append :: [a] -> [a] -> [a]
-append = todo
+append a b = foldr f b a
+    where
+    f x xs = x:xs
 
 -- Разбиение списка lst на куски размером n
 -- (последний кусок может быть меньше)
 groups :: [a] -> Integer -> [[a]]
-groups lst n = todo
+groups lst n
+    | (n < 1) || (n > toInteger (length lst)) = error "Invalid group size"
+    | otherwise = reverse $ result
+        where
+        result = case (fst tpl) of 
+            [] -> snd tpl
+            _ -> (fst tpl):(snd tpl)
+        tpl = foldl f ([], []) lst
+        f (l, ll) x
+            | toInteger (length (x:l)) == n = ([], (reverse (x:l)):ll)
+            | otherwise = ((x:l), ll)
